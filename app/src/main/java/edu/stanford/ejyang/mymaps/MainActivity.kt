2b460b5ac3f31473bd683cc10c398f99c635ad14
ejyang7 <ejyang7@gmail.com>
2020-10-18 23:24:27 -0700

    Added test comments

--- app/src/main/java/edu/stanford/ejyang/mymaps/MainActivity.kt
+++ app/src/main/java/edu/stanford/ejyang/mymaps/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Set layout manager on the recycler view
+        //Set adapter on the recycler view
     }
 }
