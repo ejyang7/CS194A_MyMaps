7195bada01cb5df081bee2f352cecb580de02c98
ejyang7 <ejyang7@gmail.com>
2020-10-24 10:21:32 -0700

    Added creating a new map pt 2 (up to 1:20:00 from video)

--- app/src/main/java/edu/stanford/ejyang/mymaps/MainActivity.kt
+++ app/src/main/java/edu/stanford/ejyang/mymaps/MainActivity.kt
@@ -1,11 +1,18 @@
 package edu.stanford.ejyang.mymaps
 
 import android.app.Activity
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import edu.stanford.ejyang.mymaps.models.Place
 import edu.stanford.ejyang.mymaps.models.UserMap
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,15 +22,19 @@ const val EXTRA_MAP_TITLE = "EXTRA_MAP_TITLE"
 private const val TAG = "MainActivity"
 private const val REQUEST_CODE = 1234
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var userMaps: MutableList<UserMap>
+    private lateinit var mapAdapter: MapsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val userMaps = generateSampleData()
+        userMaps = generateSampleData().toMutableList()
         //Set layout manager on the recycler view
         rvMaps.layoutManager = LinearLayoutManager(this)
         //Set adapter on the recycler view
-        rvMaps.adapter = MapsAdapter(this, userMaps, object: MapsAdapter.OnClickListener{
+        mapAdapter = MapsAdapter(this, userMaps, object: MapsAdapter.OnClickListener{
             override fun onItemClick(position: Int) {
                 Log.i(TAG, "onItemClick $position")
                 //When user taps on view in RV, nagivate to new activity
@@ -31,20 +42,47 @@ class MainActivity : AppCompatActivity() {
                 intent.putExtra(EXTRA_USER_MAP, userMaps[position])
                 startActivity(intent)
             }
-
         })
+        rvMaps.adapter = mapAdapter
 
         fabCreateMap.setOnClickListener{
             Log.i(TAG, "Tap on FAB")
+            showAlertDialog()
+        }
+    }
+
+    private fun showAlertDialog() {
+        val mapFormView = LayoutInflater.from(this).inflate(R.layout.dialog_create_map, null)
+        val dialog =
+            AlertDialog.Builder(this)
+                .setTitle("Map title")
+                .setView(mapFormView)
+                .setNegativeButton("Cancel", null)
+                .setPositiveButton("OK", null)
+                .show()
+
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener {
+            val title = mapFormView.findViewById<EditText>(R.id.etTitle).text.toString()
+            if (title.trim().isEmpty()){
+                Toast.makeText(this, "Map must have non-empty title", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            //Navigate to create map activity
             val intent = Intent(this@MainActivity, CreateMapsActivity::class.java)
-            intent.putExtra(EXTRA_MAP_TITLE, "new map name")
+            intent.putExtra(EXTRA_MAP_TITLE, title)
             startActivityForResult(intent, REQUEST_CODE)
+            dialog.dismiss()
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK){
             //get new map data from the data
+            val userMap = data?.getSerializableExtra(EXTRA_USER_MAP) as UserMap
+            Log.i(TAG, "onActivityResult with new map title ${userMap.title}")
+            userMaps.add(userMap)
+            mapAdapter.notifyItemInserted(userMaps.size - 1)
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
